51fdf35d5646c97c572852c738ace10ad4397336
OmarShebl36 <OmarShebl36@outlook.com>
2023-08-15 11:59:44 +0300

    feat: build the carList component

--- src/components/CarList.tsx
+++ src/components/CarList.tsx
@@ -1,5 +1,35 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { Car } from './Cars.types';
+import { removeCar } from '../store';
+
 function CarList() {
-    return ( <div>CarList</div> );
+  const dispatch = useDispatch();
+  const cars: Car[] = useSelector((state: any) => {
+    return state.cars.data;
+  });
+
+  const handleCarDelete = (
+    car: Car
+  ): void => {
+    dispatch(removeCar(car.id));
+  };
+
+  const renderedCars = () => {
+    return cars.map((car: Car) => {
+      return (
+        <div className='panel' key={car.id}>
+          <p>
+            {car.name} - ${car.cost}
+          </p>
+          <button className='button is-danger' onClick={() => handleCarDelete(car)}>
+            Delete
+          </button>
+        </div>
+      );
+    });
+  };
+
+  return <div className='car-list'>{renderedCars()}</div>;
 }
 
 export default CarList;

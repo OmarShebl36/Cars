733981f00832724a08ea267a54e31d77c58c6624
OmarShebl36 <OmarShebl36@outlook.com>
2023-08-15 16:41:09 +0300

    feat: add total value component

--- src/components/CarValue.tsx
+++ src/components/CarValue.tsx
@@ -1,5 +1,16 @@
+import { useSelector } from 'react-redux';
+import { Car } from './Cars.types';
+
 function CarValue() {
-    return ( <div>CarValue</div> );
+  const totalCost: number = useSelector(({ cars: { data, searchTerm } }) =>
+    data
+      .filter((car: Car) =>
+        car.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      .reduce((acc: number, car: Car) => acc + car.cost, 0)
+  );
+
+  return <div className='car-value'>Total cost: ${totalCost}</div>;
 }
 
 export default CarValue;

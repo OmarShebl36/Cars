11ee93eaf3c53a72b69a6c923374bb2be1dfa07e
OmarShebl36 <OmarShebl36@outlook.com>
2023-08-15 12:27:34 +0300

    feat: create the search component

--- src/components/CarSearch.tsx
+++ src/components/CarSearch.tsx
@@ -1,5 +1,33 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { changeSearchTerm } from '../store';
+import React from 'react';
+
 function CarSearch() {
-    return ( <div>CarSearch</div> );
+  const dispatch = useDispatch();
+  const searchTerm = useSelector(({ cars: { searchTerm } }) => {
+    return searchTerm;
+  });
+
+  const handleTermChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(changeSearchTerm(event?.target.value));
+  };
+
+  return (
+    <div>
+      <div className='list-header'>
+        <h3 className='title is-3'>My Cars</h3>
+        <div className='search field is-horizontal'>
+          <label className='label'>Search</label>
+          <input
+            className='input'
+            placeholder='Search term...'
+            value={searchTerm}
+            onChange={handleTermChange}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CarSearch;
